1b26d6d16c8512df6a8a7df8f647f58eb41f7723
zengjinju <zengjinju@qccr.com>
2017-03-15 19:44:35 +0800

    添加logback.xml日志配置文件

--- nb-biz/src/main/java/com/zjj/quartz/TwoBean.java
+++ nb-biz/src/main/java/com/zjj/quartz/TwoBean.java
@@ -1,14 +1,17 @@
 package com.zjj.quartz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by admin on 2017/3/13.
  */
 @Service
+@Slf4j
 public class TwoBean {
 
     public void out(){
+        log.info("twoBean test");
         System.out.println("twoBean test");
     }
 }
