d2acf4cba1e519c9c0388e928ecf4c51a8912f1d
zengjinju <zengjinju@qccr.com>
2017-03-15 19:38:50 +0800

    添加logback.xml日志配置文件

--- nb-biz/src/main/java/com/zjj/quartz/QuartzJob.java
+++ nb-biz/src/main/java/com/zjj/quartz/QuartzJob.java
@@ -1,23 +1,20 @@
 package com.zjj.quartz;
 
+import lombok.Setter;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.stereotype.Service;
 
 /**
- * Created by admin on 2017/3/13.
+ * Created by jinju.zeng on 2017/3/13.
  */
 public class QuartzJob extends QuartzJobBean {
 
+    @Setter
     private TwoBean twoBean;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         twoBean.out();
-    }
-
-    public void setTwoBean(TwoBean twoBean) {
-        this.twoBean = twoBean;
     }
 }
