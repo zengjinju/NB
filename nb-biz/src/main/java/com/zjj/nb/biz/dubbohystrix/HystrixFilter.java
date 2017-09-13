5b2e87f4e88365d1340ef83bc04a41bec45d59b4
zengjinju <zengjinju@qccr.com>
2017-09-13 17:10:23 +0800

    新增Dubbo整合hystrix熔断器的逻辑，在调用Dubbo远程服务的时候封装一层熔断器

--- nb-biz/src/main/java/com/zjj/nb/biz/dubbohystrix/HystrixFilter.java
+++ nb-biz/src/main/java/com/zjj/nb/biz/dubbohystrix/HystrixFilter.java
@@ -1,11 +1,14 @@
 package com.zjj.nb.biz.dubbohystrix;
 
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 
 /**
  * Created by jinju.zeng on 2017/9/13.
  * 对Dubbo的远程服务调用封装熔断器模式
  */
+@Activate(group = Constants.CONSUMER)
 public class HystrixFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
